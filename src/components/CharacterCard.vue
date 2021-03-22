4e23b16a9d419c2b933a37fec05c79aca83a4f50
Antonio <antoniobmirandagomes@gmail.com>
2021-03-22 14:06:05 -0300

    starting with character cards

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+v-card(
+    max-width="250"
+)
+    v-img(
+        height="150"
+        src="http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
+    )
+    v-card-title
+        |   Um Hero
+    v-card-text Uma descricao Uma descricao Uma descricao Uma descricao Uma descricao Uma descricao Uma descricao Uma descricao Uma descricao Uma descricao Uma descricao Uma descricao
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  v-row(justify="center")
+    div.central-block
+        v-row(justify="center")
+          character-card
+          character-card
+          character-card
+          character-card
+          character-card
+          character-card
+          character-card
+          character-card
+          character-card
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CharacterCard from '@/components/CharacterCard.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    CharacterCard
   }
 }
 </script>
+
+<style scoped>
+.central-block {
+  width: 65vw;
+}
+</style>
