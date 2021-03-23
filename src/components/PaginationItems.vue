e7ee5ae8daf3d6fb5e6dfaf9d0d67b771c21187f
Antonio <antoniobmirandagomes@gmail.com>
2021-03-23 16:53:33 -0300

    adding pagination to characters page

---
+++ src/components/PaginationItems.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  div
+    v-row
+      v-btn.ma-2(
+        v-for="page in totalPages"
+        @click="$emit('changePage', page)"
+        :class="{'disable-events': isDisabled(page)}"
+        :color="getColor(page)"
+        outlined
+        )
+        | {{ page }}
+</template>
+
+<script>
+export default {
+  props: {
+    totalPages: {
+      type: Array,
+      required: true
+    },
+    currentPage: {
+      type: Number
+    }
+  },
+  data () {
+    return {}
+  },
+  methods: {
+    getColor (page) {
+      if (this.currentPage === page) return 'blue'
+    },
+    isDisabled (page) {
+      return this.currentPage === page
+    }
+  }
+}
+</script>
+
+<style scoped>
+.disable-events {
+  pointer-events: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,37 @@
 <template lang="pug">
-  v-row(justify="center")
+div
+  v-row(justify="center").mb-5
     div.central-block
         v-row(justify="center")
-          template(v-for="character in characters")
+          template(v-for="character in getPageCharacters")
             character-card(:character="character").ma-3
+  v-divider
+  v-row(justify="center").pa-10
+      pagination-items(
+        :totalPages="getTotalPagesArray"
+        :currentPage="currentPage"
+        @changePage="changeCurrentPage"
+      )
 </template>
 
 <script>
 import CharacterCard from '@/components/CharacterCard.vue'
+import PaginationItems from '@/components/PaginationItems'
 import axios from 'axios'
 import { mapState } from 'vuex'
+// import _ from 'lodash'
 
 export default {
   name: 'Home',
   components: {
-    CharacterCard
+    CharacterCard,
+    PaginationItems
   },
   data () {
-    return {}
+    return {
+      itemsPerPage: 30,
+      currentPage: 1
+    }
   },
   beforeMount () {
     if (!this.isStoreInitialized) {
@@ -33,11 +47,36 @@ export default {
     }
   },
   computed: {
-    ...mapState(['characters', 'isStoreInitialized'])
+    ...mapState(['characters', 'isStoreInitialized']),
+    getTotalPagesArray () {
+      const totalPages = this.getTotalPagesAndResetCurrentPage()
+      return Array.from({ length: totalPages }, (_, index) => index + 1)
+    },
+    getPageCharacters () {
+      const pageCharacters = []
+      for (
+        let i = this.itemsPerPage * (this.currentPage - 1);
+        i < this.itemsPerPage * this.currentPage;
+        i++
+      ) {
+        if (this.characters[i]) pageCharacters.push(this.characters[i])
+      }
+      return pageCharacters
+    }
   },
   methods: {
+    changeCurrentPage (newCurrentPage) {
+      this.currentPage = newCurrentPage
+      window.scrollTo(0, 0)
+    },
     populateCharacters (charactersArray) {
       this.$store.commit('setCharacters', charactersArray)
+    },
+    getTotalPagesAndResetCurrentPage () {
+      this.currentPage = 1
+      const lengthPerItems = parseInt(this.characters.length / this.itemsPerPage)
+      if (this.characters.length % this.itemsPerPage === 0) return lengthPerItems
+      return lengthPerItems + 1
     }
   }
 }
